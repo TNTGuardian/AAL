8fb8775632a9095e085495b900d70e627fb4213d
TNTGuardian <66271168+TNTGuardian@users.noreply.github.com>
2023-10-03 20:48:31 -0400

    Update content.js

--- js/content.js
+++ js/content.js
@@ -57,22 +57,7 @@ export async function fetchLeaderboard() {
             return;
         }
 
-        // Verification
-        const verifier = Object.keys(scoreMap).find(
-            (u) => u.toLowerCase() === level.verifier.toLowerCase(),
-        ) || level.verifier;
-        scoreMap[verifier] ??= {
-            verified: [],
-            completed: [],
-            progressed: [],
-        };
-        const { verified } = scoreMap[verifier];
-        verified.push({
-            rank: rank + 1,
-            level: level.name,
-            score: score(rank + 1, 100, level.percentToQualify),
-            link: level.verification,
-        });
+
 
         // Records
         level.records.forEach((record) => {
@@ -107,8 +92,8 @@ export async function fetchLeaderboard() {
 
     // Wrap in extra Object containing the user and total score
     const res = Object.entries(scoreMap).map(([user, scores]) => {
-        const { verified, completed, progressed } = scores;
-        const total = [verified, completed, progressed]
+        const { completed, progressed } = scores;
+        const total = [completed, progressed]
             .flat()
             .reduce((prev, cur) => prev + cur.score, 0);
 
