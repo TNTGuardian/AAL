da2c92067a63623cc23bd324cd95bcf5445df8f8
TNTGuardian <66271168+TNTGuardian@users.noreply.github.com>
2023-10-16 10:05:45 -0400

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import Wave from './pages/Wave.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    { path: 'wave', component: Test },
 ];
