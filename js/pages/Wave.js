699e6e958f50c1577eae5c2ce02b2c7c749e122d
TNTGuardian <66271168+TNTGuardian@users.noreply.github.com>
2023-10-18 10:17:28 -0400

    Update Wave.js

--- js/pages/Wave.js
+++ js/pages/Wave.js
@@ -46,6 +46,44 @@ export default {
                     <p>(ノಠ益ಠ)ノ彡┻━┻</p>
                 </div>
             </div>
+            <div class="meta-container">
+                <div class="meta">
+                    <div class="errors" v-show="errors.length > 0">
+                        <p class="error" v-for="error of errors">{{ error }}</p>
+                    </div>
+                    <div class="og">
+                        <p class="type-label-md">Original List by Twisted</p>
+                    </div>
+                    <template v-if="editors">
+                        <h3>List Editors</h3>
+                        <ol class="editors">
+                            <li v-for="editor in editors">
+                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
+                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
+                                <p v-else>{{ editor.name }}</p>
+                            </li>
+                        </ol>
+                    </template>
+                    <h3>About The List</h3>
+                    <p>
+                        This list was made by Twisted as a way of listing off our completed Extreme/Insane Demons!
+                    </p>
+                    <p>
+                        (DISCLAIMER): Though the original list had records for both Extreme and Insane demons, this list only has Extremes
+                    </p>
+                    <p>
+                        When you have completed a level that is placed on this list (or one that hasn't) I will most likely notice it since I'm subscribed to most of you all on YouTube
+                    </p>
+                    <p>
+                        If your record has not been placed on the list within 2-3 days, please notify my dumbass
+                    </p>
+                    <p>
+                        DM me if there are any errors or changes needed to be made on the list
+                    </p>
+                     <p>
+                    </p>
+                </div>
+            </div>
         </main>
     `,
     data: () => ({
